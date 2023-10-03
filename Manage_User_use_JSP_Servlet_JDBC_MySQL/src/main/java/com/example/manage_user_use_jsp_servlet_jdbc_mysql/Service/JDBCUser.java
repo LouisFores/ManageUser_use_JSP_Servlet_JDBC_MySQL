ab780b1cfe980d4c5592fe6d90794737599cc6f6
LouisFores <146702670+LouisFores@users.noreply.github.com>
2023-10-03 22:58:56 +0700

    Execute SQL without using Transactions
    JDBC Transaction đảm bảo rằng một tập các câu lệnh SQL được thực thi như một đơn vị. Có 2 trường hợp xảy ra. Một là tất cả các câu lệnh SQL được thực thi thành công. Hai là sẽ không được thực thi thành công, khi đó trạng thái trước khi thực thi câu lệnh SQL sẽ được khôi phục.

---
+++ Manage_User_use_JSP_Servlet_JDBC_MySQL/src/main/java/com/example/manage_user_use_jsp_servlet_jdbc_mysql/Service/JDBCUser.java
@@ -0,0 +1,275 @@
+package com.example.manage_user_use_jsp_servlet_jdbc_mysql.Service;
+
+import com.example.manage_user_use_jsp_servlet_jdbc_mysql.Model.User;
+import jdk.vm.ci.code.site.Call;
+import sun.jvm.hotspot.gc.epsilon.EpsilonHeap;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCUser implements IUser{
+    private static final String  url = "jdbc:mysql://localhost:3306/quan_li_nguoi_dung";
+    private static final String username = "root";
+    private static final String password = "mySQL7122023@";
+    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, email, country) VALUES (?, ?, ?)";
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+    private static final String SQL_INSERT = "INSERT INTO employee (name, salary, created_date) VALUE (?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE employee SET salary=? WHERE name=?";
+    private static final String SQL_TABLE_CREATE = "CREATE TABLE employee"
+            + "("
+            + " ID serial,"
+            + " NAME varchar(100) NOT NULL,"
+            + " SALARY numeric(15, 2) NOT NULL,"
+            + " CREATED_DATE timestamp,"
+            + " PRIMARY KEY (ID)"
+            + ")";
+    private static final String SQL_TABLE_DROP = "DROP TABLE IF EXISTS EMPLOYEE";;
+    public JDBCUser() {}
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return  connection;
+    }
+    public void insertUser(User user) throws SQLException {
+        System.out.printf(INSERT_USERS_SQL);
+        try (Connection connection = connect(); PreparedStatement statement = connection.prepareStatement(INSERT_USERS_SQL)){
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            System.out.println(statement);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+    public User selectUser(int id) {
+        User user = null;
+        try (Connection connection = connect();
+             PreparedStatement statement = connection.prepareStatement(SELECT_USER_BY_ID);) {
+             statement.setInt(1, id);
+             System.out.println(statement);
+             ResultSet resultSet = statement.executeQuery();
+
+             while (resultSet.next()) {
+                 String name = resultSet.getString("name");
+                 String email = resultSet.getString("email");
+                 String country = resultSet.getString("country");
+                 user = new User(id, name, email, country);
+             }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return user;
+    }
+    public List<User> selectAllUsers() {
+        List<User> users = new ArrayList<>();
+        Connection connection = connect();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SELECT_ALL_USERS);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String country = resultSet.getString("country");
+                users.add(new User(id, name, email, country));
+                System.out.println(id + name +  email +  country);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return users;
+    }
+    public boolean deleteUser(int id) throws SQLException {
+        boolean rowDeleted;
+        try (Connection connection = connect();
+             PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);){
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+
+    public boolean updateUser(User user) throws SQLException {
+        boolean rowUpdated;
+        try (Connection connection = connect();
+             PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);){
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setInt(4, user.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.out.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.out.println("Error Code: " + e.getMessage());
+                System.out.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    @Override
+    public User getUserById(int id) {
+        User user = null;
+        String query = "{CALL get_user_by_id(?)}";
+        Connection connection = connect();
+        CallableStatement callableStatement = null;
+        try {
+            callableStatement = connection.prepareCall(query);
+            callableStatement.setInt(1, id);
+            ResultSet resultSet = callableStatement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String country = resultSet.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return user;
+    }
+
+    @Override
+    public void insertUserStore(User user) throws SQLException {
+        String query = "{CALL insert_user(?,?,?)}";
+        Connection connection = connect();
+        CallableStatement callableStatement = null;
+        try {
+            callableStatement = connection.prepareCall(query);
+            callableStatement.setString(1, user.getName());
+            callableStatement.setString(2, user.getEmail());
+            callableStatement.setString(3, user.getCountry());
+            callableStatement.executeUpdate();
+            System.out.println(callableStatement);
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    @Override
+    public void addUserTransaction(User user, List<Integer> permissions) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        PreparedStatement statementAssignment = null;
+        ResultSet resultSet = null;
+        try {
+           connection = connect();
+           // Set auto commit to false
+           connection.setAutoCommit(false);
+           // Insert user
+           statement = connection.prepareStatement(INSERT_USERS_SQL,Statement.RETURN_GENERATED_KEYS);
+           statement.setString(1, user.getName());
+           statement.setString(2, user.getEmail());
+           statement.setString(3, user.getCountry());
+           int rowAffected = statement.executeUpdate();
+           // get user id
+            resultSet = statement.getGeneratedKeys();
+
+            int userId = 0;
+            if (resultSet.next()) userId = resultSet.getInt(1);
+
+            // in case the insert operation successes, assign permission to user
+            if (rowAffected == 1) {
+                //passion permission to user
+                String sqlPivot = "INSERT INTO user_permission(user_id, permission_id)" + "VALUES(?,?)";
+                statementAssignment = connection.prepareStatement(sqlPivot);
+
+                for (int permissionId : permissions) {
+                    statementAssignment.setInt(1, userId);
+                    statementAssignment.setInt(2, permissionId);
+                    statementAssignment.executeUpdate();
+                }
+
+                connection.commit();
+            } else {
+                connection.rollback();
+            }
+
+        } catch (SQLException e) {
+            // roll back the transaction
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (statementAssignment != null) statementAssignment.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void insertUpdateWithoutTransaction() {
+        Connection connection = connect();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            PreparedStatement psInsert = connection.prepareStatement(SQL_INSERT);
+            PreparedStatement psUpdate = connection.prepareStatement(SQL_UPDATE);
+
+            statement.execute(SQL_TABLE_DROP);
+            statement.execute(SQL_TABLE_CREATE);
+
+            // Run list of insert commands
+            psInsert.setString(1, "Quynh");
+            psInsert.setBigDecimal(2, new BigDecimal(10));
+            psInsert.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            psInsert.execute();
+
+            psInsert.setString(1, "Ngan");
+            psInsert.setBigDecimal(2, new BigDecimal(20));
+            psInsert.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            psInsert.execute();
+
+
+            // Run list of update commands
+            // below line caused error, test transaction
+            // org.postgresql.util.PSQLException: No value specified for parameter 1.
+            psUpdate.setBigDecimal(2, new BigDecimal(999.99));
+            //psUpdate.setBigDecimal(1, new BigDecimal(999.99));
+            psUpdate.setString(2, "Quynh");
+            psUpdate.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
