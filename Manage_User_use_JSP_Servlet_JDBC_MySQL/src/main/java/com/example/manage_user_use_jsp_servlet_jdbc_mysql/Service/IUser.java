7028a6b8b4fe5a6742b732998b1ce77f6d994c76
LouisFores <146702670+LouisFores@users.noreply.github.com>
2023-10-03 16:31:58 +0700

    commit_rollback
    Sử dụng phương thức commit() và rollback() của đối tượng Connection để điều khiển transaction. Xây dựng phương thức mới để thực thi transaction.

---
+++ Manage_User_use_JSP_Servlet_JDBC_MySQL/src/main/java/com/example/manage_user_use_jsp_servlet_jdbc_mysql/Service/IUser.java
@@ -0,0 +1,17 @@
+package com.example.manage_user_use_jsp_servlet_jdbc_mysql.Service;
+
+import com.example.manage_user_use_jsp_servlet_jdbc_mysql.Model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUser {
+    public void insertUser(User user) throws SQLException;
+    public User selectUser(int id);
+    public List<User> selectAllUsers();
+    public boolean deleteUser(int id) throws SQLException;
+    public boolean updateUser(User user) throws SQLException;
+    public User getUserById(int id);
+    public void insertUserStore(User user) throws SQLException;
+    public void addUserTransaction(User user, List<Integer> permissions);
+}
